46a27ed50fae852f7e5a692a05677fb601b75289
Gareth Maddock <gareth.maddock@lloydwarwick.com>
2019-01-11 09:34:38 +0000

    First attempt to determine how many seasons is too many

--- scripts/20190108 tv imdb/code.R
+++ scripts/20190108 tv imdb/code.R
@@ -9,4 +9,24 @@ library(lubridate)
 #               "scripts/20190108 tv imdb/data/data.csv", mode = "wb")
 
 data <-
-  read_csv("scripts/20190108 tv imdb/data/data.csv")
+  read_csv("scripts/20190108 tv imdb/data/data.csv") %>% 
+  mutate_at(vars(seasonNumber), as.factor)
+
+data_rejigged <-
+  data %>% 
+  arrange(titleId, seasonNumber) %>%
+  group_by(titleId) %>% 
+  filter(any(seasonNumber == 1) & 
+           max(as.integer(seasonNumber)) == n() &
+           n() > 1 &
+           any(share >= 2.5)) %>%
+  mutate(rolling_mean_rating = cumsum(av_rating)/as.integer(seasonNumber)) %>% 
+  filter(titleId != "tt0106179")  # exclude the X files, as it is skewing things massively
+
+test <-
+  lm(data_rejigged$share ~ data_rejigged$seasonNumber)
+
+data_rejigged %>% 
+  gather(category, value, av_rating, share) %>% 
+  ggplot(aes(x = seasonNumber, y = value, colour = category, group = title)) +
+  geom_line(lwd = 1.2)
