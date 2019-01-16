ad4099c7840680a34a767f277cfb637d37c186d9
Gareth Maddock <gareth.maddock@lloydwarwick.com>
2019-01-16 08:20:57 +0000

    Space Launches TT (2018-01-16) completed

--- scripts/20190114 space launches/code.R
+++ scripts/20190114 space launches/code.R
@@ -4,25 +4,41 @@ library(extrafont)
 
 launches <-
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-15/launches.csv" %>% 
-  read_csv() %>% 
-  left_join("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-15/agencies.csv" %>% 
-              read_csv() %>% 
-              distinct(agency, short_name, name))
+  read_csv()
 
-launches %>% 
-  count(agency_type, launch_year) %>% 
-  ggplot(aes(launch_year, n, fill = str_to_title(agency_type))) + 
-  geom_bar(stat = "identity") +
+plot_data <-
+  launches %>% 
+  mutate(country = fct_collapse(state_code,
+                                "Russia/USSR" = c("RU", "SU"),
+                                "United States" = "US",
+                                "China" = "CN",
+                                "France" = "F",
+                                "Japan" = "J"),
+         country = fct_lump(country, 5)) %>% 
+  group_by(country, launch_year) %>% 
+  count() 
+
+plot_data %>% 
+  ggplot(aes(launch_year, n)) + 
+  geom_bar(aes(fill = country), stat = "identity") +
   scale_x_continuous(breaks = seq(1960, 2010, 10)) +
-  scale_y_continuous(expand = c(0,0)) +
-  scale_fill_viridis_d() +
   theme(rect = element_rect(fill = "#0a1926"), 
         panel.grid = element_blank(), 
         text = element_text(colour = "white", 
-                            family = "Barlow Condensed", size = 26), 
-        panel.background = element_rect("#0a1926"), legend.position = "bottom") +
-  labs(title = "The Rise of Private Space Agencies",
-       subtitle = "",
+                            family = "Tw Cen MT Condensed", size = 26), 
+        panel.background = element_rect("#0a1926"), 
+        legend.position = c(0.8,0.9), 
+        legend.text = element_text(size = 16), 
+        legend.key = element_rect(fill = "transparent", colour = "transparent"),
+        axis.text = element_text(colour = rgb(230,230,230, maxColorValue = 255), size = 16),
+        axis.title = element_text(size = 20),
+        plot.subtitle = element_text(size = 20),
+        plot.caption = element_text(size = 14)) +
+  labs(title = "The Space Races",
+       subtitle = "The countries demonstrating how humanity's reach still exceeds its grasp",
        y = "Number of Launches",
        x = "Launch Year",
-       fill = "Agency Type")
+       fill = "",
+       caption = "Source: https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-01-15") + scale_fill_brewer(palette = "RdBu")
+
+ggsave(p, filename = "scripts/20190114 space launches/space_races.png", dpi = 300, width = 10, height = 7)
