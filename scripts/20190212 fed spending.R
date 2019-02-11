8e9236ecb16b3038fc55e6d7252b1c42998953bf
gtm19 <9137502+gtm19@users.noreply.github.com>
2019-02-11 21:30:30 +0000

    created scripts for Fed spending

---
+++ scripts/20190212 fed spending.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(lubridate)
+library(janitor)
+
+
+# Read in the data --------------------------------------------------------
+
+fed_rd <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/fed_r_d_spending.csv")
+energy_spend <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/energy_spending.csv")
+climate_spend <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/climate_spending.csv")
+
